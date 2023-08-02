5c1e5235b0845ef96649382edb9c67f2d672b3f0
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-02 15:04:00 -0400

    after open closed principale

--- GlobomanticsHR/src/main/java/hr/taxes/TaxCalculator.java
+++ GlobomanticsHR/src/main/java/hr/taxes/TaxCalculator.java
@@ -1,39 +1,7 @@
 package hr.taxes;
 
 import hr.personnel.Employee;
-import hr.personnel.FullTimeEmployee;
-import hr.personnel.Intern;
-import hr.personnel.PartTimeEmployee;
 
-public class TaxCalculator {
-    private final int RETIREMENT_TAX_PERCENTAGE = 10;
-    private final int INCOME_TAX_PERCENTAGE = 16;
-    private final int BASE_HEALTH_INSURANCE = 100;
-
-
-    public double calculate(Employee employee) {
-        int monthlyIncome = employee.getMonthlyIncome();
-
-        if (employee instanceof FullTimeEmployee) {
-            return BASE_HEALTH_INSURANCE +
-                    (monthlyIncome * RETIREMENT_TAX_PERCENTAGE) / 100 +
-                    (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
-        }
-
-        if (employee instanceof PartTimeEmployee) {
-            return BASE_HEALTH_INSURANCE +
-                    (monthlyIncome * 10) / 100 +
-                    (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
-        }
-
-        if (employee instanceof Intern) {
-            if (monthlyIncome < 350) {
-                return 0;
-            } else {
-                return (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
-            }
-        }
-
-        return 0;
-    }
+public interface TaxCalculator {
+    double calculate(Employee employee);
 }
