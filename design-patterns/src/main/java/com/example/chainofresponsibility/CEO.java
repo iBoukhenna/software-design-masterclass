348ffa7a608b0d6041bc92e8c3da742ec62b2890
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-03 19:52:35 -0400

    Behavioral : Chain of Responsibility

---
+++ design-patterns/src/main/java/com/example/chainofresponsibility/CEO.java
@@ -0,0 +1,9 @@
+package com.example.chainofresponsibility;
+
+public class CEO extends Handler {
+
+	@Override
+	public void handleRequest(Request request) {
+		System.out.println("CEOs can approve anything they want");
+	}
+}
---
+++ design-patterns/src/main/java/com/example/chainofresponsibility/RequestType.java
@@ -0,0 +1,5 @@
+package com.example.chainofresponsibility;
+
+public enum RequestType {
+	CONFERENCE, PURCHASE;
+}
