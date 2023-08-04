14aa8e694510de41e778ed0481bffa586e5e0ba6
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-04 11:11:55 -0400

    Creational : Singleton <=> volatile & Threadsafe with synchronized

--- design-patterns/src/main/java/com/example/singleton/DbSingleton.java
+++ design-patterns/src/main/java/com/example/singleton/DbSingleton.java
@@ -2,14 +2,22 @@ package com.example.singleton;
 
 public class DbSingleton {
 
-	private static DbSingleton instance = null;
+	private static volatile DbSingleton instance = null;
 	
 	private DbSingleton() {
+		
+		if(instance != null) {
+			throw new RuntimeException("Use getInstance() method to create");
+		}
 	}
 	
 	public static DbSingleton getInstance() {
 		if(instance == null) {
-			instance = new DbSingleton();
+			synchronized(DbSingleton.class) {
+				if(instance == null) {
+					instance = new DbSingleton();
+				}
+			}
 		}
 		
 		return instance;
