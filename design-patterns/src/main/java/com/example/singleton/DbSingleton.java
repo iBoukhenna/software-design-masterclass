e0accb172e507c7d41a5d21ef219612b616d47d1
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-04 11:23:52 -0400

    Creational : Singleton with Database Connection

--- design-patterns/src/main/java/com/example/singleton/DbSingleton.java
+++ design-patterns/src/main/java/com/example/singleton/DbSingleton.java
@@ -1,10 +1,27 @@
 package com.example.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.derby.jdbc.EmbeddedDriver;
+
 public class DbSingleton {
 
 	private static volatile DbSingleton instance = null;
+	private static volatile Connection conn = null;
 	
 	private DbSingleton() {
+		
+		try {
+			DriverManager.registerDriver(new EmbeddedDriver());			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(conn != null) {
+			throw new RuntimeException("Use getConnection() method to create");
+		}
 		
 		if(instance != null) {
 			throw new RuntimeException("Use getInstance() method to create");
@@ -21,5 +38,23 @@ public class DbSingleton {
 		}
 		
 		return instance;
+	}
+	
+	public Connection getConnection() {
+		if(conn == null) {
+			synchronized (DbSingleton.class) {
+				if(conn == null) {
+					try {
+						String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+						
+						conn = DriverManager.getConnection(dbUrl);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		return conn;
 	}
 }
