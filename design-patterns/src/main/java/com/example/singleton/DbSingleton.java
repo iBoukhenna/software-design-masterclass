18cc6d8b80fe532fbc4295fed03745ca4add71f9
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-04 11:09:49 -0400

    Creational : Singleton & Lazy Loading

--- design-patterns/src/main/java/com/example/singleton/DbSingleton.java
+++ design-patterns/src/main/java/com/example/singleton/DbSingleton.java
@@ -2,12 +2,16 @@ package com.example.singleton;
 
 public class DbSingleton {
 
-	private static DbSingleton instance = new DbSingleton();
+	private static DbSingleton instance = null;
 	
 	private DbSingleton() {
 	}
 	
 	public static DbSingleton getInstance() {
+		if(instance == null) {
+			instance = new DbSingleton();
+		}
+		
 		return instance;
 	}
 }
