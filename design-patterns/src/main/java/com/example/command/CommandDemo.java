c04854e4da5c7b3e188c71911d684fcc6a0c91e3
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-08-03 20:55:43 -0400

    Behavioral : Command with Marco

--- design-patterns/src/main/java/com/example/command/CommandDemo.java
+++ design-patterns/src/main/java/com/example/command/CommandDemo.java
@@ -1,16 +1,20 @@
 package com.example.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandDemo {
 
 	public static void main(String args[]) {
 		Light bedroomLight = new Light();
+		Light kitchenLight = new Light();
 		Switch lightSwitch = new Switch(); 
 		
-		Command toggleCommand = new ToggleCommand(bedroomLight);
+		List<Light> lights = new ArrayList<>();
+		lights.add(kitchenLight);
+		lights.add(bedroomLight);
+		Command allLightsCommand = new AllLightsCommand(lights);
 		
-		lightSwitch.storeAndExecute(toggleCommand);
-		lightSwitch.storeAndExecute(toggleCommand);
-		lightSwitch.storeAndExecute(toggleCommand);
-		
+		lightSwitch.storeAndExecute(allLightsCommand);
 	}
 }
